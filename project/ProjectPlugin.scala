799935a5111c97ad3388f4d36405badf0fadc2d1
Scala Steward <me@scala-steward.org>
2019-09-20 18:57:42 +0200

    Update circe-core, circe-generic, ... to 0.12.1

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -16,7 +16,7 @@ object ProjectPlugin extends AutoPlugin {
       val mu             = "0.18.0"
       val pubSub         = "1.70.0"
       val pureconfig     = "0.10.2"
-      val circeVersion   = "0.11.1"
+      val circeVersion   = "0.12.1"
     }
   }
 
