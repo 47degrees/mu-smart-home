f20621e4c999f3bec7ffbf56225af641330a9e9b
Scala Steward <me@scala-steward.org>
2019-12-21 06:06:42 +0100

    Update pureconfig to 0.12.2

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -15,7 +15,7 @@ object ProjectPlugin extends AutoPlugin {
       val logbackClassic = "1.2.3"
       val mu             = "0.18.0"
       val pubSub         = "1.102.0"
-      val pureconfig     = "0.12.1"
+      val pureconfig     = "0.12.2"
       val circeVersion   = "0.12.3"
     }
   }
