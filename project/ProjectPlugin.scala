e4bb0ced1ed5d594662457a90d98aad6dd6fcd4b
47erbot <developer@47deg.com>
2021-07-16 07:11:19 +0200

    Update circe-core, circe-generic, ... to 0.14.1

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -16,7 +16,7 @@ object ProjectPlugin extends AutoPlugin {
       val logbackClassic = "1.2.3"
       val mu             = "0.21.3"
       val pureconfig     = "0.14.1"
-      val circeVersion   = "0.13.0"
+      val circeVersion   = "0.14.1"
       val scala          = "2.13.1"
       val fs2PubSub      = "0.17.0"
     }
